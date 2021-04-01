9eae9a1135d23cec5c3e0ff8250d4a2ba6722692
SiggiSigmann <siggisigmann@googlemail.com>
2021-04-01 14:03:35 +0200

    add code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GPS_Fixer
 Can be used to find a gps fix for GPS moduled (tested with BN220)
+
+# Hardware
+A 2x16 LCD is used to display used Satelites, Longtitute and Latitute for each connected gps Modules
+
+The LCD is connected to Pins rs = 6, en = 7, d4 = 5, d5 = 4, d6 = 3, d7 = 2,
+GPS1 is connected to rx = 10, tx = 11
+GPS2 is connected to rx = 8, tx = 9
---
+++ src/main.cpp
@@ -0,0 +1,126 @@
+#include <SoftwareSerial.h>
+#include <Arduino.h>
+#include <TinyGPS++.h>
+#include <LiquidCrystal.h>
+
+#define BAUTRATE 9600
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 6, en = 7, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+//rx, tx
+SoftwareSerial port1(10, 11);
+TinyGPSPlus gps1;
+SoftwareSerial port2(8, 9);
+TinyGPSPlus gps2;
+
+struct GPSStruct {
+    SoftwareSerial *soft;
+    TinyGPSPlus gps;
+    String name;
+    int row;
+};
+
+GPSStruct gpsstruct[2];
+
+
+//other variables
+float GPSLat;
+float GPSLon;
+float GPSAlt;
+uint8_t GPSSats;
+uint32_t GPSHdop;
+uint32_t GPSstarttimemS;
+uint32_t GPSendtimemS;
+
+//method declaration
+void printGPS(TinyGPSPlus);
+void displayGPS(TinyGPSPlus, int, String);
+void readSoftSerail(GPSStruct);
+
+void setup() {
+  gpsstruct[0].soft = &port1;
+  gpsstruct[0].gps = gps1;
+  gpsstruct[0].name = "GPS1";
+  gpsstruct[0].row = 0;
+
+  gpsstruct[1].soft = &port2;
+  gpsstruct[1].gps = gps2;
+  gpsstruct[1].name = "GPS2";
+  gpsstruct[1].row = 1;
+
+  // LCD
+  lcd.begin(16, 2);
+
+  //Serial communication
+  Serial.begin(BAUTRATE);
+  while (!Serial) {}
+
+  //software serial
+  port1.begin(BAUTRATE);
+  port2.begin(BAUTRATE);
+}
+
+void loop() {
+  readSoftSerail(gpsstruct[0]);
+  readSoftSerail(gpsstruct[1]);
+}
+
+void readSoftSerail(GPSStruct gps_struct){
+  Serial.print(gps_struct.name + ": ");
+
+  gps_struct.soft->listen();
+  while (gps_struct.soft->available() > 0) {
+    char inByte = gps_struct.soft->read();
+    gps_struct.gps.encode(inByte);
+  }
+
+  printGPS(gps_struct.gps);
+  displayGPS(gps_struct.gps, gps_struct.row, gps_struct.name);
+  Serial.println();
+}
+
+void displayGPS(TinyGPSPlus gps, int row, String name){
+  GPSSats = gps.satellites.value();
+
+  lcd.setCursor(0, row);
+  lcd.print(name);
+
+  lcd.setCursor(5, row);
+  lcd.print(GPSSats);
+
+  GPSLat = gps.location.lat();
+  GPSLon = gps.location.lng();
+
+  lcd.setCursor(7, row);
+  lcd.print(GPSLat);
+
+  lcd.setCursor(11, row);
+  lcd.print(" ");
+  lcd.print(GPSLon);
+}
+
+void printGPS(TinyGPSPlus gps){
+  float tempfloat;
+
+  GPSLat = gps.location.lat();
+  GPSLon = gps.location.lng();
+  GPSAlt = gps.altitude.meters();
+  GPSSats = gps.satellites.value();
+  GPSHdop = gps.hdop.value();
+  tempfloat = ( (float) GPSHdop / 100);
+
+  Serial.print(F("Lat,"));
+  Serial.print(GPSLat, 6);
+  Serial.print(F(",Lon,"));
+  Serial.print(GPSLon, 6);
+  Serial.print(F(",Alt,"));
+  Serial.print(GPSAlt, 1);
+  Serial.print(F("m,Sats,"));
+  Serial.print(GPSSats);
+  Serial.print(F(",HDOP,"));
+  Serial.print(tempfloat, 2);
+  Serial.println();
+}
