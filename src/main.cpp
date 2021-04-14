0769bb01000960754e2fd4e69d83d74ca4fa0a38
SiggiSigmann <siggisigmann@googlemail.com>
2021-04-14 13:00:32 +0200

    add time formating

--- src/main.cpp
+++ src/main.cpp
@@ -403,6 +403,24 @@ void display_multi_1(TinyGPSPlus gps, short name){
   }
 }
 
+//calculate day per month
+int  getNumberOfDays(int month, int year){
+	//leap year condition, if month is 2
+	if( month == 2){
+		if((year%400==0) || (year%4==0 && year%100!=0)){
+			return 29;
+    }else{
+			return 28;
+    }
+	}
+	//months which has 31 days
+	else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8||month == 10 || month==12){
+		return 31;
+  }else {		
+		return 30;
+  }
+}
+
 //time and date display
 void display_multi_2(TinyGPSPlus gps, short name){
   short hour, dayoffset = 0;
@@ -414,6 +432,23 @@ void display_multi_2(TinyGPSPlus gps, short name){
     if(dayoffset) hour-=24;
   }
 
+  short month = gps.date.month();
+  short year = gps.date.year();
+  short days = gps.date.day()+dayoffset;
+  short dayPerMonth = getNumberOfDays(month, year);
+
+  //check if day fits to month and offset month
+  if(dayPerMonth < days){
+    month++;
+    days = days-dayPerMonth;
+  }
+
+  //offset year
+  if(month > 12){
+    month = 1;
+    year++;
+  }
+
   //first line
   lcd.setCursor(0, 0);
   lcd.print(name);
@@ -421,17 +456,17 @@ void display_multi_2(TinyGPSPlus gps, short name){
   //date
   lcd.print("Date ");
   if(gps.date.isValid()){
-    if(gps.date.day()+dayoffset < 10){
+    if(days < 10){
       lcd.print(" ");
     }
-    lcd.print(gps.date.day()+dayoffset);
+    lcd.print(days);
     lcd.print(".");
-    if(gps.date.month() < 10){
+    if(month < 10){
       lcd.print("0");
     }
-    lcd.print(gps.date.month());
+    lcd.print(month);
     lcd.print(".");
-    lcd.print(gps.date.year());
+    lcd.print(year);
     lcd.print("        ");
   }else{
     lcd.print("--.--.----");
